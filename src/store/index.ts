55bf32f0a162fb4e6e751c2547e66fdd66983cae
myvega2k <vega2k@kakao.com>
2023-09-05 14:52:50 +0900

    storage object  add

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,21 @@
 import { createStore, createLogger } from "vuex"
+import { TodoItem } from "@/types/TodoItem"
+
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                const storageKey = localStorage.key(i) as string;
+                const itemJson = localStorage.getItem(storageKey) as string | null
+                if (itemJson) {
+                    arr.push(JSON.parse(itemJson))
+                }
+            }
+        }
+        return arr;
+    }
+};
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
