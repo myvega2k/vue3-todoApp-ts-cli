511497968ef3c069fc3b52a3ac4dd4606a4a776f
myvega2k <vega2k@kakao.com>
2023-09-05 13:55:31 +0900

    todoinput update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,6 +4,17 @@
         <span class="addContainer" @click="addTodo">
             <i class="fas fa-plus addBtn"></i>
         </span>
+        <MyModal v-if="showModal" @close="showModal = false">
+            <template v-slot:header>
+                <h3>
+                    경고!
+                    <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+                </h3>
+            </template>
+            <template v-slot:body>
+                <div>아무것도 입력하지 않으셨습니다.</div>
+            </template>
+        </MyModal>
     </div>
 </template>
 
